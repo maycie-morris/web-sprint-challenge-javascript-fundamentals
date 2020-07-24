e57513016a30694f5132dac4823ba3ba87d6e68c
Maycie Morris <maycie@MacBook-Pro.local>
2020-07-24 08:40:31 -0700

    AC Task 3

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -20,8 +20,15 @@ const zooAnimals = [
 The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
-const displayNames = [];
+
+const displayNames = []
+
+const whatever = zooAnimals.forEach(function (item) {
+  return displayNames.push(item.animal_name + item.scientific_name)
+})
+
 console.log(displayNames);
+
 
 /* Request 2: .map()
 
@@ -29,7 +36,10 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames
+const lowCaseAnimalNames = zooAnimals.map(function (item) {
+  return item.animal_name.toLowerCase();
+});
+
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
@@ -37,16 +47,20 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals
+
+const lowPopulationAnimals = zooAnimals.filter(function (item) {
+  return item.population < 5
+});
+
 console.log(lowPopulationAnimals);
 
-/* Request 4: .reduce() 
+/* Request 4: .reduce()
 
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-let populationTotal = 0;
-console.log(populationTotal);
+// let populationTotal = 0;
+// console.log(populationTotal);
 
 
 // ==== Callbacks ====  
@@ -61,7 +75,7 @@ console.log(populationTotal);
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
-  * Create a function named multiply that returns the product of two numbers 
+  * Create a function named multiply that returns the product of two numbers
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
