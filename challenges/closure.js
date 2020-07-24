fd96c2c47dcc5232d80b4457103655d93d155e40
Maycie Morris <maycie@MacBook-Pro.local>
2020-07-24 09:08:34 -0700

    Task 1 closure

--- challenges/closure.js
+++ challenges/closure.js
@@ -18,7 +18,10 @@ myFunction();
 
 // Explanation: 
 
+// Nested function can access the variable internal because they are both within the lexical scope of the function myFunction.
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+
